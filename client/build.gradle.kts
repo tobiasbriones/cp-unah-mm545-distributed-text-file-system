6457e42f9eced3fda67666e57d577d6dc13aa17b
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-06-11 16:18:42 -0600

    Add JavaFX modules

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     java
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.0.9"
 }
 
 group = "io.github.tobiasbriones.cp"
@@ -12,6 +14,11 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+javafx {
+    version = "15.0.1"
+    modules = listOf("javafx.controls", "javafx.fxml")
 }
 
 tasks.getByName<Test>("test") {
---
+++ client/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module cp.unah.mm545.distributed.text.file.system.client.main {
+    requires javafx.graphics;
+
+    opens io.github.tobiasbriones.cp.rmifilesystem;
+}
