d677379364f3db602785d881941f248440300834
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-06-12 18:44:57 -0600

    Import javafx.fxml

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module io.github.tobiasbriones.cp.rmifilesystem.client {
     requires javafx.graphics;
     requires javafx.controls;
+    requires javafx.fxml;
 
     opens io.github.tobiasbriones.cp.rmifilesystem;
 }
