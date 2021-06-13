61d6fe72a3e8f80056ab77a38b7b0ca94f970710
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-06-13 11:55:19 -0600

    Add license notice header

--- client/src/main/java/io/github/tobiasbriones/cp/rmifilesystem/ui/App.java
+++ client/src/main/java/io/github/tobiasbriones/cp/rmifilesystem/ui/App.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This file is part of Course Project at UNAH-MM545: Distributed Text File
+ * System.
+ *
+ * This source code is licensed under the BSD-3-Clause License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/BSD-3-Clause.
+ */
+
 package io.github.tobiasbriones.cp.rmifilesystem.ui;
 
 import io.github.tobiasbriones.cp.rmifilesystem.ui.content.Content;
