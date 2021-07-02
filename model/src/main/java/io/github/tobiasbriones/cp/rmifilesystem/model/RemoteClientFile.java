858cba66a8fdd5b8d8f8ea922bcf1fbd9ac34e09
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-07-02 17:30:27 -0600

    Add RemoteClientFile

---
+++ model/src/main/java/io/github/tobiasbriones/cp/rmifilesystem/model/RemoteClientFile.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This file is part of Course Project at UNAH-MM545: Distributed Text File
+ * System.
+ *
+ * This source code is licensed under the BSD-3-Clause License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/BSD-3-Clause.
+ */
+
+package io.github.tobiasbriones.cp.rmifilesystem.model;
+
+import java.io.File;
+
+/**
+ * @author Tobias Briones
+ */
+public class RemoteClientFile extends ClientFile {
+    // TODO Add remote context
+    public RemoteClientFile(File file) {
+        super(file);
+    }
+
+    @Override
+    public final boolean isFile() {
+        // TODO
+        return false;
+    }
+
+    @Override
+    public final boolean isDirectory() {
+        // TODO
+        return false;
+    }
+
+    public final LocalClientFile toLocalClientFile(String rootPath) {
+        return LocalClientFile.fromClientFile(this, rootPath);
+    }
+}
