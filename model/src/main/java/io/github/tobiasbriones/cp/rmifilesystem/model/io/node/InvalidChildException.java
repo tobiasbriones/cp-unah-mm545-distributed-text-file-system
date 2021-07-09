9116a6b051ffc1ccbb7c606e4f48f7f75d45ba72
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-07-09 13:12:49 -0600

    Define InvalidChildException

---
+++ model/src/main/java/io/github/tobiasbriones/cp/rmifilesystem/model/io/node/InvalidChildException.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This file is part of Course Project at UNAH-MM545: Distributed Text File
+ * System.
+ *
+ * This source code is licensed under the BSD-3-Clause License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/BSD-3-Clause.
+ */
+
+package io.github.tobiasbriones.cp.rmifilesystem.model.io.node;
+
+import io.github.tobiasbriones.cp.rmifilesystem.model.io.CommonFile;
+import io.github.tobiasbriones.cp.rmifilesystem.model.io.Directory;
+
+import java.io.Serial;
+
+/**
+ * @author Tobias Briones
+ */
+public final class InvalidChildException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 2032717105378391322L;
+
+    private static String getMessage(Directory parent, CommonFile invalidChild) {
+        return """
+               Invalid child %s for parent %s
+               """.formatted(invalidChild, parent);
+    }
+
+    InvalidChildException(Directory parent, CommonFile invalidChild) {
+        super(getMessage(parent, invalidChild));
+    }
+}
