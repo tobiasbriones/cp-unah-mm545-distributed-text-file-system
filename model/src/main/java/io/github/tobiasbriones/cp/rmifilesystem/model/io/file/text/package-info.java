e3d98614cce9e55099ee1914fd1d1c27ffec937a
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-07-14 19:21:58 -0600

    Add package: text

--- model/src/main/java/io/github/tobiasbriones/cp/rmifilesystem/model/io/file/text/package-info.java
+++ model/src/main/java/io/github/tobiasbriones/cp/rmifilesystem/model/io/file/text/package-info.java
@@ -14,12 +14,4 @@
 /**
  * @author Tobias Briones
  */
-module io.github.tobiasbriones.cp.rmifilesystem.model {
-    requires java.rmi;
-
-    exports io.github.tobiasbriones.cp.rmifilesystem.model;
-    exports io.github.tobiasbriones.cp.rmifilesystem.model.io;
-    exports io.github.tobiasbriones.cp.rmifilesystem.model.io.node;
-    exports io.github.tobiasbriones.cp.rmifilesystem.model.io.file;
-    exports io.github.tobiasbriones.cp.rmifilesystem.model.io.file.text;
-}
+package io.github.tobiasbriones.cp.rmifilesystem.model.io.file.text;
