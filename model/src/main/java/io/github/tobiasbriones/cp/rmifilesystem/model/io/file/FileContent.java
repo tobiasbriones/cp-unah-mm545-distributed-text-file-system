75f57320af497934a8761a12ee2f6d1817823cc8
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-07-14 19:23:18 -0600

    Define FileContent and TextFileContent
    Make a representation for a CommonFile (a file) and its content (e.g., a TextFile which contains a String as its content)

---
+++ model/src/main/java/io/github/tobiasbriones/cp/rmifilesystem/model/io/file/FileContent.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This file is part of Course Project at UNAH-MM545: Distributed Text File
+ * System.
+ *
+ * This source code is licensed under the BSD-3-Clause License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/BSD-3-Clause.
+ */
+
+package io.github.tobiasbriones.cp.rmifilesystem.model.io.file;
+
+import io.github.tobiasbriones.cp.rmifilesystem.model.io.CommonFile;
+
+/**
+ * @author Tobias Briones
+ */
+public interface FileContent<T extends CommonFile, C> {
+    T file();
+
+    C content();
+}
